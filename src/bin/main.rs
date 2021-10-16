35818c71de0c47aea70a858438c9cb93ac94ec67
Syed Huzaifa <rustingwork@gmail.com>
2021-10-17 00:04:35 +0500

    WorkSpace

---
+++ src/bin/main.rs
@@ -0,0 +1,24 @@
+use notify::{watcher, RecursiveMode, Watcher};
+use std::{sync::mpsc::channel,time::Duration};
+
+
+
+fn main() -> anyhow::Result<()> {
+    let (tx,rx) = channel();
+
+    let mut watcher = 
+    watcher(tx, Duration::from_secs(5))?;
+    
+
+    // Directory Path
+    watcher.watch("D:/Rust",RecursiveMode::Recursive)?; 
+    
+    // printing the event 
+    while let Ok(event) =
+     rx.recv(){
+        println!("Working on Event{:?}", event);
+    }
+    
+     
+   Ok(()) 
+}
